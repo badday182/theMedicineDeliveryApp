6d8aa29cd17ef26ea9afb97dbe277c5c1e3d050c
Valerii <badday182@gmail.com>
2024-03-10 18:01:05 +0200

    add form to confirm delivery

--- app/src/components/cartCards/CartCards.jsx
+++ app/src/components/cartCards/CartCards.jsx
@@ -1,60 +1,12 @@
-// import { Card, Form } from "react-bootstrap";
-// import Col from "react-bootstrap/Col";
-// import Row from "react-bootstrap/Row";
-// import Button from "react-bootstrap/Button";
-// import cardsData from "../cardsData/cardsData";
-// import { useDispatch, useSelector } from "react-redux";
-// import { resetMedicinesArray } from "../../redux/slices/cartSliseReducer";
-
-// const CartCards = () => {
-//   const dispatch = useDispatch();
-
-//   const cartMedicinesArray = useSelector((state) => state.cart.medicinesArray);
-
-//   // console.log(cartMedicinesArray);
-
-//   return (
-//     <div className="my-2 p-2 border border-primary-subtle rounded-2">
-//       <Row xs={2} md={4} className="g-3 mb-2">
-//         {cardsData.map((card, idx) => (
-//           <Col key={idx}>
-//             <Card>
-//               <Card.Img variant="top" src={card.imageUrl} />
-//               <Card.Body>
-//                 <div className="p-2 d-flex align-items-center justify-content-between flex-wrap">
-//                   <Card.Title>{card.title}</Card.Title>
-//                   <div className="w-50">
-//                     <Form.Control
-//                       type="number"
-//                       id="inputPassword5"
-//                     />
-//                   </div>
-//                 </div>
-//                 <Button variant="danger">Remove</Button>
-//               </Card.Body>
-//             </Card>
-//           </Col>
-//         ))}
-//       </Row>
-//       <Button
-//         variant="danger"
-//         onClick={() => {
-//           dispatch(resetMedicinesArray());
-//         }}
-//       >
-//         Clear Cart
-//       </Button>
-//     </div>
-//   );
-// };
-// export default CartCards;
-
 import { Card, Form } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { useDispatch, useSelector } from "react-redux";
-import { removeFromMedicinesArray, resetMedicinesArray } from "../../redux/slices/cartSliseReducer";
+import {
+  removeFromMedicinesArray,
+  resetMedicinesArray,
+} from "../../redux/slices/cartSliseReducer";
 import cardsData from "../cardsData/cardsData";
 
 const CartCards = () => {
@@ -66,39 +18,80 @@ const CartCards = () => {
     cartMedicinesArray.includes(card.title)
   );
 
-  const cardButtonOnClick = (title)=> {
+  const cardButtonOnClick = (title) => {
     // console.log(title)
-    dispatch(removeFromMedicinesArray(title))
-}
+    dispatch(removeFromMedicinesArray(title));
+  };
 
   return (
-    <div className="my-2 p-2 border border-primary-subtle rounded-2">
-      <Row xs={2} md={4} className="g-3 mb-2">
-        {cartCardsData.map((card, idx) => (
-          <Col key={idx}>
-            <Card>
-              <Card.Img variant="top" src={card.imageUrl} />
-              <Card.Body>
-                <div className="p-2 d-flex align-items-center justify-content-between flex-wrap">
-                  <Card.Title>{card.title}</Card.Title>
-                  <div className="w-50">
-                    <Form.Control type="number" id="inputPassword5" defaultValue={1}/>
-                  </div>
-                </div>
-                <Button variant="danger" onClick={()=>{cardButtonOnClick(card.title)}}>Remove</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      <Button
-        variant="danger"
-        onClick={() => {
-          dispatch(resetMedicinesArray());
-        }}
-      >
-        Clear Cart
-      </Button>
+    <div className="my-2 p-2 pt-3 border border-primary-subtle rounded-2">
+      <div>
+        <Form>
+          <Row xs={2} md={4} className="g-3 mb-2">
+            {cartCardsData.map((card, idx) => (
+              <Col key={idx}>
+                <Card>
+                  <Card.Img variant="top" src={card.imageUrl} />
+                  <Card.Body>
+                    <div className="p-2 d-flex align-items-center justify-content-between flex-wrap">
+                      <Card.Title>{card.title}</Card.Title>
+                      <div className="w-50">
+                        <Form.Control
+                          type="number"
+                          id="inputPassword5"
+                          defaultValue={1}
+                        />
+                      </div>
+                    </div>
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        cardButtonOnClick(card.title);
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Name" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridPhone">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control type="tel" placeholder="0939998877" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridAddress">
+              <Form.Label>Address</Form.Label>
+              <Form.Control placeholder="1234 Main St" />
+            </Form.Group>
+          </Row>
+          <Button className="mb-3" variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <div>
+        <Button
+          variant="danger"
+          onClick={() => {
+            dispatch(resetMedicinesArray());
+          }}
+        >
+          Clear Cart
+        </Button>
+      </div>
     </div>
   );
 };
