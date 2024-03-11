20716ba7a95015a370770dc5cc383d16ca0e898e
Valerii <badday182@gmail.com>
2024-03-11 08:40:35 +0200

    add Analytics

--- app/src/components/home/Home.js
+++ app/src/components/home/Home.js
@@ -1,8 +1,10 @@
 import { Container, Image } from "react-bootstrap"
+import { Analytics } from "@vercel/analytics/react"
 
 const Home = () => {
     return (
 <div className="my-2 p-2 border border-primary-subtle rounded-2 ">
+<Analytics/>
 <Container className="text-center">
    <h1 className="my-4 ">
    Wellcome to Medicine Delivery app
